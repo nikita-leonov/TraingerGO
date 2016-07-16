144a7cebb5310b06a2cf53ac06911386f6fca86a
nikita.leonov <nikita.leonov@huami-usa.com>
2016-07-15 18:59:20 -0700

    Initial Commit

---
+++ GoTrainer/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GoTrainer
+//
+//  Created by Nikita Leonov on 7/15/16.
+//  Copyright © 2016 leonov.co. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
